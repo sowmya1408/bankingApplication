b28f9c2d8e4f8a8e64310e7fa8963aa7a7056412
Morten Laursen <mortendlaursen@gmail.com>
2020-10-26 14:49:34 +0100

    now all should be fixed

--- 2_database/account.js
+++ 2_database/account.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-=======
 // 3. Finish the account schema
->>>>>>> fd52b477a525e76101ad4d05035f1269ae80b659
 const AccountSchema = new mongoose.Schema({
     /**
      * the schema follows this structure:
@@ -20,17 +17,6 @@ const AccountSchema = new mongoose.Schema({
     firstName: {
         type: String,
         required: true,
-<<<<<<< HEAD
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    balance: {
-        type: Number,
-        required: true,
-=======
->>>>>>> fd52b477a525e76101ad4d05035f1269ae80b659
     }
 });
 
